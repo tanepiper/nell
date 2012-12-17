bee9a7f555694609fcbb83d3f581c001b006ddb9
Tane Piper <piper.tane@gmail.com>
2012-12-17 14:10:24 +0000

    Added i18n support

---
+++ locales/en.js
@@ -0,0 +1,14 @@
+{
+	"Overide the theme used in the nell.json file": "Overide the theme used in the nell.json file",
+	"Generates a new site": "Generates a new site",
+	"Create a new page with title as a quote encased string (e.g. \"My New Page\")": "Create a new page with title as a quote encased string (e.g. \"My New Page\")",
+	"Create a new post with title as a quote encased string (e.g. \"My New Post\")": "Create a new post with title as a quote encased string (e.g. \"My New Post\")",
+	"Lists all available plugins for this site": "Lists all available plugins for this site",
+	"Preview the current site in the output folder with an Express static server": "Preview the current site in the output folder with an Express static server",
+	"Site %s created in %s": "Site %s created in %s",
+	"Site preview at http://127.0.0.1:%d": "Site preview at http://127.0.0.1:%d",
+	"Generate a static website from the nell.json and source files in this directory": "Generate a static website from the nell.json and source files in this directory",
+	"Provides date formatting tools, see https://github.com/felixge/node-dateformat for full docs": "Provides date formatting tools, see https://github.com/felixge/node-dateformat for full docs",
+	"Site \"%s\" generated in %ds": "Site \"%s\" generated in %ds",
+	"Takes a gist id and optional filename and returns a gist embed": "Takes a gist id and optional filename and returns a gist embed"
+}
